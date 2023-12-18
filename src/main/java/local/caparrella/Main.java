f5801311e220c529cdf3d5d76cd5a0f5bdecfa17
JuliaCarullaDAM <jcarulla2@inscaparrella.cat>
2023-12-18 19:35:25 +0100

    el julio fara el merge cap al main d'aquesta branca

--- src/main/java/local/caparrella/Main.java
+++ src/main/java/local/caparrella/Main.java
@@ -6,9 +6,10 @@ public class Main {
         Persona[] persones = new Persona[3];
 
         // Inicialitza els objectes a l'array
-        persones[0] = new Persona("Júlia", 19);
-        persones[1] = new Persona("Júlia", 19);
-        persones[1] = new Persona("Júlia", 19);
+        persones[0] = new Persona("Júlia", 25);
+        persones[1] = new Persona("Maria", 30);
+        persones[1] = new Persona("Pepe", 30);
+        System.out.println("Aquesta branca està feta per la Júlia, i el Julio farà un merge al main!");
       
 
         // Accedeix als objectes a l'array
